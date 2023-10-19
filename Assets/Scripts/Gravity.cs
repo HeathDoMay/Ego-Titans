d99b291b9ed4f8cef7a3d9129cfd8ce98ce87c0e
Carson McMahan <carson.mcmahan@gmail.com>
2023-10-18 19:49:42 -0600

    Fixing Gravity
    Creating a work around so players are not controlling each others cameras.

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -11,13 +11,11 @@ public class Gravity : MonoBehaviour
     private Rigidbody rb;
     private float gravity = 5f;
     private float gravFix = 0f;
-    private Quaternion rot;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rot = transform.rotation;
         rb.useGravity = false;
 
         //transform.rotation = Quaternion.Euler(0f, 180f, 0f);
@@ -30,44 +28,98 @@ public class Gravity : MonoBehaviour
         rb.AddForce(Physics.gravity, ForceMode.Acceleration);
     }
 
-    private void OnGravityToggle(InputValue value)
+    private void Update()
+    {
+        Test();
+    }
+
+    //private void OnGravityToggle()
+    //{
+    //    var transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+    //    if (gravity > 0 && gravFix == 1f)
+    //    {
+    //        Debug.Log(gravFix);
+    //        gravFix += 1f;
+    //        gravity = -5f;
+    //        //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+    //        transposer.m_FollowOffset = new Vector3(0, 3, -10);
+
+    //    }
+    //    else if (gravity > 0 && gravFix == 3f)
+    //    {
+    //        Debug.Log(gravFix);
+    //        gravFix = 0f;
+    //        gravity = -5f;
+    //        //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+    //        transposer.m_FollowOffset = new Vector3(0, 3, -10);
+    //    }
+    //    else if (gravity < 0 && gravFix == 0f)
+    //    {
+    //        Debug.Log(gravFix);
+    //        gravFix += 1f;
+    //        gravity = 5f;
+    //        //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+    //        transposer.m_FollowOffset = new Vector3(0, -3f, -10);
+    //    }
+    //    else if (gravity < 0 && gravFix == 2f)
+    //    {
+    //        Debug.Log(gravFix);
+    //        gravFix += 1f;
+    //        gravity = 5f;
+    //        //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+    //        transposer.m_FollowOffset = new Vector3(0, -3f, -10);
+    //    }
+
+    //    Physics.gravity = new Vector3(0, gravity, 0);
+    //}
+
+    private void Test()
     {
         var transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
 
-        if (gravity > 0 && gravFix == 1f)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            Debug.Log(gravFix);
-            gravFix += 1f;
-            gravity = -5f;
-            //transform.Rotate(0f, rot.y * 180f - 180, 180f);
-            transposer.m_FollowOffset = new Vector3(0, 3, -10);
+            if (gravity > 0 && gravFix == 1f)
+            {
+                Debug.Log(gravFix);
+                gravFix += 1f;
+                gravity = -5f;
+                //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+                transposer.m_FollowOffset = new Vector3(0, 3, -10);
 
-        }
-        else if (gravity > 0 && gravFix == 3f)
-        {
-            Debug.Log(gravFix);
-            gravFix = 0f;
-            gravity = -5f;
-            //transform.Rotate(0f, rot.y * 180f - 180, 180f);
-            transposer.m_FollowOffset = new Vector3(0, 3, -10);
-        }
-        else if (gravity < 0 && gravFix == 0f)
-        {
-            Debug.Log(gravFix);
-            gravFix += 1f;
-            gravity = 5f;
-            //transform.Rotate(0f, rot.y * 180f - 180, 180f);
-            transposer.m_FollowOffset = new Vector3(0, -3f, -10);
-        }
-        else if (gravity < 0 && gravFix == 2f)
-        {
-            Debug.Log(gravFix);
-            gravFix += 1f;
-            gravity = 5f;
-            //transform.Rotate(0f, rot.y * 180f - 180, 180f);
-            transposer.m_FollowOffset = new Vector3(0, -3f, -10);
+            }
+            else if (gravity > 0 && gravFix == 3f)
+            {
+                Debug.Log(gravFix);
+                gravFix = 0f;
+                gravity = -5f;
+                //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+                transposer.m_FollowOffset = new Vector3(0, 3, -10);
+            }
+            else if (gravity < 0 && gravFix == 0f)
+            {
+                Debug.Log(gravFix);
+                gravFix += 1f;
+                gravity = 5f;
+                //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+                transposer.m_FollowOffset = new Vector3(0, -3f, -10);
+            }
+            else if (gravity < 0 && gravFix == 2f)
+            {
+                Debug.Log(gravFix);
+                gravFix += 1f;
+                gravity = 5f;
+                //transform.Rotate(0f, rot.y * 180f - 180, 180f);
+                transposer.m_FollowOffset = new Vector3(0, -3f, -10);
+            }
+
+            Physics.gravity = new Vector3(0, gravity, 0);
         }
 
-        Physics.gravity = new Vector3(0, gravity, 0);
+        if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            Debug.Log("Pressed");
+        }
     }
 }
